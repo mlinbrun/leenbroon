1fc4edab48f187e20f993acc3ab7b590fc64e7b6
mlinbrun <72449332+mlinbrun@users.noreply.github.com>
2021-08-19 14:36:56 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,16 @@
+module.exports = config = {
+    dragon: {
+        database: {
+            user: "",
+            password: "",
+            connectString: "",
+            poolIncrement: 1,
+            poolMax: 5,
+            poolMin: 5,
+            poolTimeout: 60,
+            poolAlias: "default",
+            queueMax: 500,
+            queueTimeout: 60,
+        },
+    }
+};
